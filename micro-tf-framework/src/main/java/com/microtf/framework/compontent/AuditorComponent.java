e3551138d5e3cecc50dab4b858cb5da630b60aaf
guliuzhong <glzaboy@163.com>
2022-07-12 16:04:57 +0800

    审计增加创建人，更新人，并增加自动更新人

---
+++ micro-tf-framework/src/main/java/com/microtf/framework/compontent/AuditorComponent.java
@@ -0,0 +1,31 @@
+package com.microtf.framework.compontent;
+
+import com.microtf.framework.dto.login.LoginStateDto;
+import com.microtf.framework.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+class AuditorComponent implements AuditorAware<Long> {
+    LoginService loginService;
+
+    @Autowired
+    public void setLoginService(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        loginService.initLoginUser();
+        LoginStateDto loginStateDto = loginService.getLoginStateDto();
+        if(loginStateDto.getGuest()){
+            return Optional.empty();
+        }else{
+            return Optional.of(Long.valueOf(loginStateDto.getUserId()));
+        }
+
+    }
+}
--- micro-tf-framework/src/main/java/com/microtf/framework/jpa/AuditEntity.java
+++ micro-tf-framework/src/main/java/com/microtf/framework/jpa/AuditEntity.java
@@ -4,13 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
 
+/**
+ * 基础数据表
+ * 包含数据库审计和版本功能
+ */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
@@ -19,13 +25,21 @@ public class AuditEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
+    @Column(nullable = false)
+    @Comment("主键")
     private Long id;
     @CreatedDate
     @Comment("创建时间")
     Date createTime;
+    @CreatedBy
+    @Comment("创建人ID")
+    Long createdBy;
     @LastModifiedDate
     @Comment("更新时间")
     Date updateTime;
+    @LastModifiedBy
+    @Comment("更新人ID")
+    Long updateBy;
     @Version
     @Comment("版本")
     Long version;
