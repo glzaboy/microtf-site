b384643033eb34308c4653880b5584b06b041316
glzaboy <glzaboy@163.com>
2022-07-17 19:19:40 +0800

    修改存储注册点

--- micro-tf-framework/src/main/java/com/microtf/framework/event/StorageRegister.java
+++ micro-tf-framework/src/main/java/com/microtf/framework/event/StorageRegister.java
@@ -1,23 +1,26 @@
-package com.microtf.api;
+package com.microtf.framework.event;
 
 import com.microtf.framework.services.BeanService;
 import com.microtf.framework.services.SettingService;
 import com.microtf.framework.services.storage.Config;
 import com.microtf.framework.services.storage.S3StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.Properties;
 
 /**
- * 程序启动
+ * 存储自动注册
  * @author glzaboy
  */
 @Component
-public class ApplicationStart implements ApplicationRunner {
+@Slf4j
+public class StorageRegister implements ApplicationListener<ContextRefreshedEvent> {
 
     BeanService beanService;
 
@@ -33,13 +36,16 @@ public class ApplicationStart implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) {
+    public void onApplicationEvent(@NotNull ContextRefreshedEvent event) {
+        log.info("注册存储事件");
         Map<String, Config> settingByClass = settingService.getSettingByClass(Config.class);
         for (Map.Entry<String,Config> item:settingByClass.entrySet()){
+            log.info("注册{}",item.getKey());
             Properties properties=new Properties();
             properties.put("config",item.getValue());
             properties.put("pathStart",item.getValue().getRootPath());
             beanService.register(item.getKey(), S3StorageService.class,properties);
         }
+        log.info("注册存储事件 finish");
     }
 }
