93b5f769e3c75e20ec0fc416efadb633f544bfcd
glzaboy <glzaboy@163.com>
2022-08-20 19:19:57 +0800

    存储增加ID，除上传外其它均能完ID操作。

--- micro-tf-framework/src/main/java/com/microtf/framework/services/storage/FeishuStorageService.java
+++ micro-tf-framework/src/main/java/com/microtf/framework/services/storage/FeishuStorageService.java
@@ -3,12 +3,20 @@ package com.microtf.framework.services.storage;
 import com.microtf.framework.dto.storage.StorageObject;
 import com.microtf.framework.dto.storage.StorageObjectStream;
 import com.microtf.framework.exceptions.BizException;
+import com.microtf.framework.jpa.FeishuUserTokenRepository;
+import com.microtf.framework.jpa.entity.FeishuUserToken;
 import com.microtf.framework.services.storage.feishu.RootDirInfo;
 import com.microtf.framework.services.storage.feishu.Token;
 import com.microtf.framework.services.storage.feishu.TokenInput;
 import com.microtf.framework.services.storage.feishu.UploadInfo;
 import com.microtf.framework.utils.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
@@ -16,12 +24,14 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * S3协议存储服务
@@ -59,33 +69,59 @@ public class FeishuStorageService implements StorageService {
         this.redisTemplate = redisTemplate;
     }
 
-    public BiFunction<String,String, HttpUtil.HttpAuthReturn> httpBearValue=(String user, String pwd)-> {
-        HttpUtil.HttpAuthReturn httpAuthReturn = redisTemplate.opsForValue().get(user + pwd);
-        if(httpAuthReturn!=null){
-            return httpAuthReturn;
-        }
-        HttpUtil.HttpAuthReturn httpAuthReturn1 = new HttpUtil.HttpAuthReturn();
-        HttpUtil.HttpRequest.HttpRequestBuilder builder = HttpUtil.HttpRequest.builder();
-        builder.url("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal");
-        builder.method(HttpUtil.Method.JSON).postObject(TokenInput.builder().appId(user).appSecret(pwd).build());
-        HttpUtil.HttpResponse sent = HttpUtil.sent(builder.build());
-        if(sent.getStatus() ==200){
-            Token json = sent.json(Token.class);
-            if(json.getCode()==0){
-                httpAuthReturn1.setAuthValue("Bearer "+json.getTenantAccessToken());
-                redisTemplate.opsForValue().set(user+pwd,httpAuthReturn1,json.getExpire()-1800, TimeUnit.SECONDS);
+    FeishuUserTokenRepository feishuUserTokenRepository;
+
+    @Autowired
+    public void setFeishuUserTokenRepository(FeishuUserTokenRepository feishuUserTokenRepository) {
+        this.feishuUserTokenRepository = feishuUserTokenRepository;
+    }
+
+    public Function<HttpUtil.HttpAuth, HttpUtil.HttpAuthReturn> httpBearValue=(HttpUtil.HttpAuth httpAuth)-> {
+        if(httpAuth.getOpenUserId()==null){//走应用api
+            HttpUtil.HttpAuthReturn httpAuthReturn = redisTemplate.opsForValue().get(httpAuth.getUser() + httpAuth.getPwd());
+            if(httpAuthReturn!=null){
+                return httpAuthReturn;
+            }
+            HttpUtil.HttpAuthReturn httpAuthReturn1 = new HttpUtil.HttpAuthReturn();
+            HttpUtil.HttpRequest.HttpRequestBuilder builder = HttpUtil.HttpRequest.builder();
+            builder.url("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal");
+            builder.method(HttpUtil.Method.JSON).postObject(TokenInput.builder().appId(httpAuth.getUser()).appSecret(httpAuth.getPwd()).build());
+            HttpUtil.HttpResponse sent = HttpUtil.sent(builder.build());
+            if(sent.getStatus() ==200){
+                Token json = sent.json(Token.class);
+                if(json.getCode()==0){
+                    httpAuthReturn1.setAuthValue("Bearer "+json.getTenantAccessToken());
+                    redisTemplate.opsForValue().set(httpAuth.getUser() + httpAuth.getPwd(),httpAuthReturn1,json.getExpire()-1800, TimeUnit.SECONDS);
+                    return httpAuthReturn1;
+                }
+                return null;
+            }else {
+                return null;
+            }
+        }else{
+            HttpUtil.HttpAuthReturn httpAuthReturn = redisTemplate.opsForValue().get("fsUserToken"+httpAuth.getUser() + httpAuth.getOpenUserId());
+            if(httpAuthReturn!=null){
+                return httpAuthReturn;
+            }
+            FeishuUserToken token=new FeishuUserToken();
+            token.setAppId(httpAuth.getUser());
+            token.setOpenId(httpAuth.getOpenUserId());
+            Optional<FeishuUserToken> one = feishuUserTokenRepository.findOne(Example.of(token));
+            HttpUtil.HttpAuthReturn httpAuthReturn1 = new HttpUtil.HttpAuthReturn();
+            if(one.isPresent()){
+                FeishuUserToken feishuUserToken = one.get();
+                httpAuthReturn1.setAuthValue("Bearer "+feishuUserToken.getAccessToken());
+                redisTemplate.opsForValue().set("fsUserToken"+httpAuth.getUser() + httpAuth.getOpenUserId(),httpAuthReturn1,60, TimeUnit.SECONDS);
                 return httpAuthReturn1;
             }
             return null;
-        }else {
-            return null;
         }
     };
-    public RootDirInfo getRootDir(){
+    private RootDirInfo getRootDir(){
         HttpUtil.HttpRequest.HttpRequestBuilder builder = HttpUtil.HttpRequest.builder();
         builder.url("https://open.feishu.cn/open-apis/drive/explorer/v2/root_folder/meta");
         builder.method(HttpUtil.Method.GET)
-                .auth(HttpUtil.HttpAuth.builder().user(config.getAccessKeyId()).pwd(config.getSecretAccessKey()).build())
+                .auth(HttpUtil.HttpAuth.builder().user(config.getAccessKeyId()).pwd(config.getSecretAccessKey()).openUserId(config.getOpenUserId()).build())
                 .authFunction(httpBearValue).build();
         HttpUtil.HttpResponse sent = HttpUtil.sent(builder.build());
         if(sent.getStatus().intValue()==200){
@@ -101,42 +137,30 @@ public class FeishuStorageService implements StorageService {
     }
     @Override
     public StorageObject upload(URI url, String objName) throws BizException {
-        return null;
-//        OkHttpClient.Builder httpClientBuilder=new OkHttpClient.Builder();
-//        OkHttpClient httpClient = httpClientBuilder.build();
-//        Request.Builder requestBuilder=new Request.Builder();
-//        try {
-//            requestBuilder.url(url.toURL()).get();
-//        } catch (MalformedURLException e) {
-//            log.error("S3StorageService-->upload download url {}失败 {}",url,e);
-//            throw new BizException("上传文件失败，获取远程内容失败");
-//        }
-//        try {
-//            Response execute = httpClient.newCall(requestBuilder.build()).execute();
-//            if(!execute.isSuccessful()){
-//                throw new BizException("S3StorageService-->upload response fail");
-//            }
-//            ResponseBody body = execute.body();
-//            if(body==null){
-//                log.error("S3StorageService-->upload response Body is null");
-//                throw new BizException("上传文件失败，获取远程内容失败");
-//            }
-////            InputStream inputStream = body.byteStream();
-////            PutObjectArgs.Builder pubObjectArgBuilder = PutObjectArgs.builder();
-////            pubObjectArgBuilder.bucket(config.getBucket());
-////            pubObjectArgBuilder.object(objName);
-////            pubObjectArgBuilder.stream(inputStream,inputStream.available(),-1);
-////            MediaType mediaType = body.contentType();
-////            if(mediaType!=null){
-////                pubObjectArgBuilder.contentType(mediaType.toString());
-////            }
-////            ObjectWriteResponse objectWriteResponse = getClient().putObject(pubObjectArgBuilder.build());
-//            log.info("storage upload result{},{}",objectWriteResponse.etag(),objectWriteResponse.versionId());
-//            return getUrl(objName);
-//        } catch (IOException | ServerException | InsufficientDataException | ErrorResponseException | NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException | InternalException e) {
-//            log.error("S3StorageService-->upload storage upload fail",e);
-//            throw new BizException("上传文件失败");
-//        }
+        OkHttpClient.Builder httpClientBuilder=new OkHttpClient.Builder();
+        OkHttpClient httpClient = httpClientBuilder.build();
+        Request.Builder requestBuilder=new Request.Builder();
+        try {
+            requestBuilder.url(url.toURL()).get();
+        } catch (MalformedURLException e) {
+            log.error("FeishuStorageService-->upload download url {}失败 {}",url,e);
+            throw new BizException("上传文件失败，获取远程内容失败");
+        }
+        try {
+            Response execute = httpClient.newCall(requestBuilder.build()).execute();
+            if(!execute.isSuccessful()){
+                throw new BizException("FeishuStorageService-->upload response fail");
+            }
+            ResponseBody body = execute.body();
+            if(body==null){
+                log.error("FeishuStorageService-->upload response Body is null");
+                throw new BizException("上传文件失败，获取远程内容失败");
+            }
+            return upload(body.byteStream(),objName,body.contentType().toString());
+        } catch (IOException e) {
+            log.error("FeishuStorageService-->upload storage upload fail",e);
+            throw new BizException("上传文件失败");
+        }
     }
 
     @Override
@@ -156,20 +180,20 @@ public class FeishuStorageService implements StorageService {
             postFile.put("file",HttpUtil.File.builder().contentType(contentType).fileName(objName).content(bytes).build());
             builder.method(HttpUtil.Method.FILE).url("https://open.feishu.cn/open-apis/drive/v1/files/upload_all")
                             .postFile(postFile).form(formData);
-            builder.auth(HttpUtil.HttpAuth.builder().user(config.getAccessKeyId()).pwd(config.getSecretAccessKey()).build())
+            builder.auth(HttpUtil.HttpAuth.builder().user(config.getAccessKeyId()).pwd(config.getSecretAccessKey()).openUserId(config.getOpenUserId()).build())
                     .authFunction(httpBearValue);
             HttpUtil.HttpResponse sent = HttpUtil.sent(builder.build());
             UploadInfo json = sent.json(UploadInfo.class);
             log.info("上传结果{}",json);
             return getUrl(json.getData().getFileToken());
         } catch (IOException e) {
-            log.error("S3StorageService-->upload storage upload fail",e);
+            log.error("FeishuStorageService-->upload storage upload fail",e);
             throw new BizException("上传文件失败");
         }
     }
 
     @Override
-    public void delete(String objName) throws BizException {
+    public void delete(String objectId) throws BizException {
         return;
 //        try {
 //            getClient().removeObject(RemoveObjectArgs.builder().bucket(config.getBucket()).object(objName).build());
@@ -179,7 +203,7 @@ public class FeishuStorageService implements StorageService {
 //        }
     }
     @Override
-    public List<String> delete(List<String> objNameList) throws BizException {
+    public List<String> delete(List<String> objectIdList) throws BizException {
         return null;
 //        List<DeleteObject> strings = new ArrayList<>();
 //        for(String item:objNameList){
@@ -204,36 +228,31 @@ public class FeishuStorageService implements StorageService {
     }
 
     @Override
-    public StorageObject getUrl(String objName) throws BizException {
+    public StorageObject getUrl(String objectId) throws BizException {
         StorageObject storageObject=new StorageObject();
-        storageObject.setObjectName(objName);
+        storageObject.setObjectId(objectId);
+        storageObject.setUrl(config.getUrlHost()+objectId);
         return storageObject;
     }
 
     @Override
-    public StorageObjectStream getStream(String objName) throws BizException {
-        return null;
-//        StorageObjectStream storageObjectStream=new StorageObjectStream();
-//        storageObjectStream.setObjectName(objName);
-//        Map<String,String> metaData=new HashMap<>(16);
-//        GetObjectArgs.Builder builder = GetObjectArgs.builder();
-//        builder.bucket(config.getBucket()).object(objName);
-//        try {
-//            GetObjectResponse object = getClient().getObject(builder.build());
-//            if (object != null) {
-//                Headers headers = object.headers();
-//                for (int i=0;i<headers.size();i++){
-//                    metaData.put(headers.name(i),headers.value(i));
-//                }
-//                storageObjectStream.setMetaData(metaData);
-//                storageObjectStream.setBufferedInputStream(new BufferedInputStream(object));
-//                return storageObjectStream;
-//            }else{
-//                throw new BizException("获取远程数据出错");
-//            }
-//        } catch (ServerException | InsufficientDataException | ErrorResponseException | IOException | NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException | InternalException e) {
-//            log.error("S3StorageService-->getStream fail",e);
-//            throw new BizException("获取远程数据出错");
-//        }
+    public StorageObjectStream getStream(String objectId) throws BizException {
+        StorageObjectStream storageObjectStream=new StorageObjectStream();
+        storageObjectStream.setObjectId(objectId);
+        Map<String,String> metaData=new HashMap<>(16);
+        HttpUtil.HttpRequest.HttpRequestBuilder builder = HttpUtil.HttpRequest.builder();
+        Map<String,String> pathVar=new HashMap<>();
+        pathVar.put("file_token",objectId);
+        builder.method(HttpUtil.Method.GET).url("https://open.feishu.cn/open-apis/drive/v1/files/:file_token/download");
+        builder.auth(HttpUtil.HttpAuth.builder().user(config.getAccessKeyId()).pwd(config.getSecretAccessKey()).openUserId(config.getOpenUserId()).build())
+                .authFunction(httpBearValue)
+                .pathVar(pathVar);
+        HttpUtil.HttpResponse sent = HttpUtil.sent(builder.build());
+        sent.getHeaders().forEach((item,value)->{
+            metaData.put(item,value);
+        });
+        storageObjectStream.setMetaData(metaData);
+        storageObjectStream.setBufferedInputStream(new BufferedInputStream(new ByteArrayInputStream(sent.getBody())));
+        return storageObjectStream;
     }
 }
